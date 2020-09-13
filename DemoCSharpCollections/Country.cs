4de515670e88fdb528b8bb05489a9ba836cad27a
Sophart <phunsophart@gmail.com>
2020-09-13 14:24:35 +0700

    Add Country Model

---
+++ DemoCSharpCollections/Country.cs
@@ -0,0 +1,10 @@
+namespace DemoCSharpCollections
+{
+    class Country
+    {
+        public string Name { get; set; }
+        public string Code { get; set; }
+        public string Region { get; set; }
+        public int Population { get; set; }
+    }
+}
