8670c66f4a541ee8f668bf8c9bf3d70263af36a9
Sophart Phun <sophartphun@Sopharts-MacBook-Pro.local>
2020-09-13 22:01:47 +0700

    Change l to L in PopulationFormatter

--- DemoCSharpCollections/PopulationFormatter.cs
+++ DemoCSharpCollections/PopulationFormatter.cs
@@ -19,7 +19,7 @@ namespace DemoCSharpCollections
 		{
 			// work out what rounding accuracy we need if we are to round to 
 			// 4 significant figures
-			int accuracy = Math.Max((int)(GetHighestPowerofTen(population) / 10_000l), 1);
+			int accuracy = Math.Max((int)(GetHighestPowerofTen(population) / 10_000L), 1);
 
 			// now do the rounding
 			return RoundToNearest(population, accuracy);
