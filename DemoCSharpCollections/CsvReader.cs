efdaa54290b77b292afd0c387f39b560a23c60b2
Sophart <phunsophart@gmail.com>
2020-09-13 15:09:17 +0700

    Implement ReadCountryFromCsvLine Method

--- DemoCSharpCollections/CsvReader.cs
+++ DemoCSharpCollections/CsvReader.cs
@@ -13,5 +13,15 @@ namespace DemoCSharpCollections
         {
             return null;
         }
+
+        public Country ReadCountryFromCsvLine(string csvLine)
+        {
+            string[] parts = csvLine.Split(',');
+            var name = parts[0];
+            var code = parts[1];
+            var region = parts[2];
+            var population = int.Parse(parts[3]);
+            return new Country(name, code, region, population);
+        }
     }
 }
