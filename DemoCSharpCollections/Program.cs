04d96709d12e3d0d90eb639f77cef6b78cb76861
Sophart <phunsophart@gmail.com>
2020-09-13 13:31:35 +0700

    Project Setup

---
+++ DemoCSharpCollections/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DemoCSharpCollections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
